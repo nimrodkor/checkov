b22602ab5d9cf7b58de8dbf59ba29d6701f10772
James Woolfenden <james.woolfenden@gmail.com>
2021-03-10 12:23:12 +0000

    fix test no

--- checkov/terraform/checks/resource/gcp/GKEEnableShieldedNodes.py
+++ checkov/terraform/checks/resource/gcp/GKEEnableShieldedNodes.py
@@ -4,7 +4,7 @@ from checkov.common.models.enums import CheckCategories
 class GKEEnableShieldedNodes(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure Shielded GKE Nodes are Enabled"
-        id = "CKV_GCP_66"
+        id = "CKV_GCP_71"
         supported_resources = ['google_container_cluster']
         categories = [CheckCategories.KUBERNETES]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
