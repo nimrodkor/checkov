c9ea4961c6291b122841b418edea8b6bcef4009d
Steve Vaknin <stevevaknin21@gmail.com>
2021-03-11 16:58:06 +0200

    CKV_K8S_117 k8s etcd client certificate auth

---
+++ checkov/kubernetes/checks/EtcdClientCertAuth.py
@@ -0,0 +1,24 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.kubernetes.base_spec_check import BaseK8Check
+
+class EtcdClientCertAuth(BaseK8Check):
+    def __init__(self):
+        # CIS-1.6 2.2
+        id = "CKV_K8S_117"
+        name = "Ensure that the --client-cert-auth argument is set to true"
+        categories = [CheckCategories.KUBERNETES]
+        supported_entities = ['containers']
+        super().__init__(name=name, id=id, categories=categories, supported_entities=supported_entities)
+
+    def get_resource_id(self, conf):
+        return f'{conf["parent"]} - {conf["name"]}'
+
+    def scan_spec_conf(self, conf):
+        if conf.get("command") is not None:
+            if "etcd" in conf["command"]:
+                if "--client-cert-auth=true" not in conf["command"]:
+                    return CheckResult.FAILED
+           
+        return CheckResult.PASSED
+
+check = EtcdClientCertAuth()
