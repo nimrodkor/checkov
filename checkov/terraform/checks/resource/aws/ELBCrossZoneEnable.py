d8532ecefc8ce7ed5eea4980534bf770406b1d12
suli <guysul@gmail.com>
2021-03-11 16:08:02 +0200

    Use BaseResourceValueCheck

--- checkov/terraform/checks/resource/aws/ELBCrossZoneEnable.py
+++ checkov/terraform/checks/resource/aws/ELBCrossZoneEnable.py
@@ -1,21 +1,19 @@
-from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceCheck
+from checkov.cloudformation.checks.resource.base_resource_value_check import BaseResourceValueCheck
 from checkov.common.models.enums import CheckResult, CheckCategories
 
 
-class ELBCrossZoneEnable(BaseResourceCheck):
+class ELBCrossZoneEnable(BaseResourceValueCheck):
 
     def __init__(self):
         name = "Ensure that ELB is cross-zone-load-balancing enabled"
         id = "CKV_AWS_138"
         supported_resources = ['aws_elb']
         categories = [CheckCategories.NETWORKING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_block_result=CheckResult.PASSED)
 
-    def scan_resource_conf(self, conf):
-        if "cross_zone_load_balancing" in conf:
-            if not conf["cross_zone_load_balancing"]:
-                return CheckResult.FAILED
-        return CheckResult.PASSED
+    def get_inspected_key(self):
+        return 'cross_zone_load_balancing'
 
 
 check = ELBCrossZoneEnable()
