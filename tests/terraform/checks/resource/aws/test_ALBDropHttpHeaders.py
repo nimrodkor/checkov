0f834ce72d11e062fe18eef4802d12b1743c0482
suli <guysul@gmail.com>
2021-03-11 14:47:54 +0200

    Change to alb instead od lb resource

--- tests/terraform/checks/resource/aws/test_ALBDropHttpHeaders.py
+++ tests/terraform/checks/resource/aws/test_ALBDropHttpHeaders.py
@@ -10,7 +10,7 @@ class TestLBDeletionProtection(unittest.TestCase):
 
     def test_failure(self):
         hcl_res = hcl2.loads("""
-                resource "aws_lb" "test_failed" {
+                resource "aws_alb" "test_failed" {
                     name               = "test-lb-tf"
                     internal           = false
                     load_balancer_type = "network"
@@ -18,26 +18,26 @@ class TestLBDeletionProtection(unittest.TestCase):
                     drop_invalid_header_fields = false
                 }
                 """)
-        resource_conf = hcl_res['resource'][0]['aws_lb']['test_failed']
+        resource_conf = hcl_res['resource'][0]['aws_alb']['test_failed']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_failure_missing_attribute(self):
         hcl_res = hcl2.loads("""
-                   resource "aws_lb" "test_failed" {
+                   resource "aws_alb" "test_failed" {
                        name               = "test-lb-tf"
                        internal           = false
                        load_balancer_type = "network"
                        subnets            = aws_subnet.public.*.id
                    }
                    """)
-        resource_conf = hcl_res['resource'][0]['aws_lb']['test_failed']
+        resource_conf = hcl_res['resource'][0]['aws_alb']['test_failed']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_success(self):
         hcl_res = hcl2.loads("""
-               resource "aws_lb" "test_success" {
+               resource "aws_alb" "test_success" {
                     name               = "test-lb-tf"
                     internal           = false
                     load_balancer_type = "network"
@@ -45,7 +45,7 @@ class TestLBDeletionProtection(unittest.TestCase):
                     drop_invalid_header_fields = true
                 }
                 """)
-        resource_conf = hcl_res['resource'][0]['aws_lb']['test_success']
+        resource_conf = hcl_res['resource'][0]['aws_alb']['test_success']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
