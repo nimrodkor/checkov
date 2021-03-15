dff223eb7028ef6c303d54292d0a278cc8f2be03
yehuda livnoni <yehuda@bridgecrew.io>
2021-03-15 12:08:18 +0200

    change aws_network_interface to aws_instance

--- tests/terraform/checks/resource/aws/test_EC2EBSOptimized.py
+++ tests/terraform/checks/resource/aws/test_EC2EBSOptimized.py
@@ -9,22 +9,22 @@ class TestEC2EBSOptimized(unittest.TestCase):
 
     def test_failure(self):
         hcl_res = hcl2.loads("""
-            resource "aws_network_interface" "foo" {
+            resource "aws_instance" "foo" {
               subnet_id   = some_id
             }
         """)
-        resource_conf = hcl_res['resource'][0]['aws_network_interface']['foo']
+        resource_conf = hcl_res['resource'][0]['aws_instance']['foo']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_success_default(self):
         hcl_res = hcl2.loads("""
-            resource "aws_network_interface" "foo" {
+            resource "aws_instance" "foo" {
               subnet_id     = some_id
               ebs_optimized = true
             }
         """)
-        resource_conf = hcl_res['resource'][0]['aws_network_interface']['foo']
+        resource_conf = hcl_res['resource'][0]['aws_instance']['foo']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.PASSED, scan_result)
 
