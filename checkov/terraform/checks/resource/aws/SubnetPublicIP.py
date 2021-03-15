5b4a4ef8315a535ce2556630f2d6f4815a988682
yehuda livnoni <yehuda@bridgecrew.io>
2021-03-15 11:36:09 +0200

    using BaseResourceNegativeValueCheck instaed of BaseResourceCheck

--- checkov/terraform/checks/resource/aws/SubnetPublicIP.py
+++ checkov/terraform/checks/resource/aws/SubnetPublicIP.py
@@ -1,23 +1,20 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class SubnetPublicIP(BaseResourceCheck):
+class SubnetPublicIP(BaseResourceNegativeValueCheck):
+    def get_forbidden_values(self):
+        return [True]
+
+    def get_inspected_key(self):
+        return "map_public_ip_on_launch"
+
     def __init__(self):
-        name = "Ensure VPC subnets do not assign a public IP by default"
+        name = "Ensure VPC subnets do not assign public IP by default"
         id = "CKV_AWS_130"
         supported_resources = ['aws_subnet']
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
-
-    def scan_resource_conf(self, conf):
-        key="map_public_ip_on_launch"
-        if key in conf.keys():
-            if not conf[key][0]:
-                return CheckResult.PASSED
-            return CheckResult.FAILED
-        else:
-            return CheckResult.PASSED
 
 
 check = SubnetPublicIP()
