9379acf8991dfcf87aeb696e602633e63a060189
hadarlevian <peer.hadar@gmail.com>
2021-03-15 11:21:54 +0200

    Fix

--- checkov/terraform/checks/resource/aws/RDSEnhancedMonitorEnabled.py
+++ checkov/terraform/checks/resource/aws/RDSEnhancedMonitorEnabled.py
@@ -1,21 +1,20 @@
 from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
 
-class RDSEnhancedMonitorEnabled(BaseResourceNegativeValueCheck):
+class RDSEnhancedMonitorEnabled(BaseResourceValueCheck):
     def __init__(self):
         name = "Ensure that enhanced monitoring is enabled for Amazon RDS instances"
         id = "CKV_AWS_118"
-        supported_resources = ['aws_db_instance']
+        supported_resources = ['aws_db_instance', 'aws_rds_cluster_instance']
         categories = [CheckCategories.LOGGING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
-                         missing_attribute_result=CheckResult.FAILED)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def get_inspected_key(self):
         return 'monitoring_interval'
 
-    def get_forbidden_values(self):
-        return [0]
+    def get_expected_values(self):
+        return [1, 5, 10, 15, 30, 60]
 
 
 check = RDSEnhancedMonitorEnabled()
--- tests/terraform/checks/resource/aws/test_RDSEnhancedMonitorEnabled.py
+++ tests/terraform/checks/resource/aws/test_RDSEnhancedMonitorEnabled.py
@@ -44,7 +44,26 @@ resource "aws_db_instance" "default" {
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
 
-    def test_success2(self):
+    def test_failure3(self):
+        hcl_res = hcl2.loads("""
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  monitoring_interval  = "5"
+}
+        """)
+        resource_conf = hcl_res['resource'][0]['aws_db_instance']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
+
+    def test_success1(self):
         hcl_res = hcl2.loads("""
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
@@ -57,6 +76,25 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   monitoring_interval  = 5
+  }
+           """)
+        resource_conf = hcl_res['resource'][0]['aws_db_instance']['default']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.PASSED, scan_result)
+
+    def test_success2(self):
+        hcl_res = hcl2.loads("""
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  monitoring_interval  = 15
   }
            """)
         resource_conf = hcl_res['resource'][0]['aws_db_instance']['default']
