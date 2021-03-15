a28cb37b9740c6ce70fbf070bad3a273334f8cd1
Nir Adler <nir@bridgecrew.io>
2021-03-15 15:30:20 +0200

    CKV_K8S_114

--- tests/kubernetes/checks/test_SchedulerProfiling.py
+++ tests/kubernetes/checks/test_SchedulerProfiling.py
@@ -1,18 +1,18 @@
 import os
 import unittest
 
-from checkov.kubernetes.checks.SchedulerBindAddress import check
+from checkov.kubernetes.checks.SchedulerProfiling import check
 from checkov.kubernetes.runner import Runner
 from checkov.runner_filter import RunnerFilter
 
 
-class TestSchedulerBindAddress(unittest.TestCase):
+class TestSchedulerProfiling(unittest.TestCase):
 
     def test_summary(self):
         runner = Runner()
         current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        test_files_dir = current_dir + "/example_SchedulerBindAddress"
+        test_files_dir = current_dir + "/example_SchedulerProfiling"
         report = runner.run(root_folder=test_files_dir, runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
