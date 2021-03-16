2c624a725a259322adbf0d28b358968a1ddb7271
Nir Adler <nir@bridgecrew.io>
2021-03-16 12:41:43 +0200

    format

--- checkov/kubernetes/checks/KubletEventCapture.py
+++ checkov/kubernetes/checks/KubletEventCapture.py
@@ -10,17 +10,18 @@ class KubletEventCapture(BaseK8Check):
         name = "Ensure that the --event-qps argument is set to 0 or a level which ensures appropriate event capture"
         categories = [CheckCategories.KUBERNETES]
         supported_entities = ['containers']
-        super().__init__(name=name, id=id, categories=categories, supported_entities=supported_entities)
+        super().__init__(name=name, id=id, categories=categories,
+                         supported_entities=supported_entities)
 
     def get_resource_id(self, conf):
         return f'{conf["parent"]} - {conf["name"]}'
 
     def scan_spec_conf(self, conf):
         if "command" in conf:
-            if "kubelet" in conf["command"]:     
+            if "kubelet" in conf["command"]:
                 for cmd in conf["command"]:
                     if "=" in cmd:
-                        [key,value] =   cmd.split("=")
+                        [key, value] = cmd.split("=")
                         if key == "--event-qps":
                             if int(value) > 5:
                                 return CheckResult.FAILED
@@ -28,4 +29,4 @@ class KubletEventCapture(BaseK8Check):
         return CheckResult.PASSED
 
 
-check =  KubletEventCapture()
+check = KubletEventCapture()
