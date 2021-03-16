5e095198de845383e93f3e0469ad3d4d9f80fce0
hadarlevian <peer.hadar@gmail.com>
2021-03-16 13:21:44 +0200

    Fix

--- checkov/terraform/checks/resource/azure/AppServiceDisallowCORS.py
+++ checkov/terraform/checks/resource/azure/AppServiceDisallowCORS.py
@@ -6,7 +6,7 @@ class AppServiceDisallowCORS(BaseResourceNegativeValueCheck):
     def __init__(self):
         name = "Ensure that CORS disallows every resource to access app services"
         id = "CKV_AZURE_57"
-        supported_resources = ['azurerm_function_app']
+        supported_resources = ['azurerm_app_service']
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources, missing_attribute_result=CheckResult.PASSED)
 
