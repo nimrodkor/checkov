01b4f48075185465eb790bfffc0c4945650429a9
hadarlevian <peer.hadar@gmail.com>
2021-03-16 15:22:31 +0200

    CKV_AZURE_71

--- checkov/terraform/checks/resource/azure/AppServiceIdentityProviderEnabled.py
+++ checkov/terraform/checks/resource/azure/AppServiceIdentityProviderEnabled.py
@@ -5,7 +5,7 @@ from checkov.terraform.checks.resource.base_resource_value_check import BaseReso
 
 class AppServiceIdentityProviderEnabled(BaseResourceValueCheck):
     def __init__(self):
-        name = "Ensure that 'Net Framework' version is the latest, if used as a part of the web app"
+        name = "Ensure that Managed identity provider is enabled for app services"
         id = "CKV_AZURE_71"
         supported_resources = ['azurerm_app_service']
         categories = [CheckCategories.GENERAL_SECURITY]
