fb082f38102db596bd6535bc93b516d5eae4832b
hadarlevian <peer.hadar@gmail.com>
2021-03-17 14:09:12 +0200

    CKV_AZURE_112

---
+++ checkov/terraform/checks/resource/azure/KeyBackedByHSM.py
@@ -0,0 +1,20 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+
+
+class KeyBackedByHSM(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure that key vault key is backed by HSM"
+        id = "CKV_AZURE_112"
+        supported_resources = ['azurerm_key_vault_key']
+        categories = [CheckCategories.BACKUP_AND_RECOVERY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'key_type'
+
+    def get_expected_values(self):
+        return ['EC-HSM', 'RSA-HSM']
+
+
+check = KeyBackedByHSM()
