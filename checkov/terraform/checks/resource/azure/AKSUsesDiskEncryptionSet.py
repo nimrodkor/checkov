6006d4a6f2695167997422251bfa66b08b156530
Or Evron <orevron@gmail.com>
2021-03-17 15:30:16 +0200

    fix

--- checkov/terraform/checks/resource/azure/AKSUsesDiskEncryptionSet.py
+++ checkov/terraform/checks/resource/azure/AKSUsesDiskEncryptionSet.py
@@ -1,21 +1,21 @@
-from checkov.common.models.consts import ANY_VALUE
-from checkov.common.models.enums import CheckCategories
-from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_negative_value_check import BaseResourceNegativeValueCheck
 
 
-class AKSUsesDiskEncryptionSet(BaseResourceValueCheck):
+class AKSUsesDiskEncryptionSet(BaseResourceNegativeValueCheck):
     def __init__(self):
         name = "Ensure that AKS uses disk encryption set"
         id = "CKV_AZURE_117"
         supported_resources = ['azurerm_kubernetes_cluster']
         categories = [CheckCategories.NETWORKING]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources,
+                         missing_attribute_result=CheckResult.FAILED)
 
     def get_inspected_key(self):
         return "disk_encryption_set_id"
 
-    def get_expected_value(self):
-        return ANY_VALUE
+    def get_forbidden_values(self):
+        return ['']
 
 
 check = AKSUsesDiskEncryptionSet()
