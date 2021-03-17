984fac7ed89531b5928b32722a6f91807abfa6d6
milkana <milkana@bridgecrew.io>
2021-03-17 17:00:40 +0200

    CKV_AZURE_129

---
+++ checkov/terraform/checks/resource/azure/MariaDBGeoBackupEnabled.py
@@ -0,0 +1,23 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceCheck
+
+
+class MariaDBGeoBackupEnabled(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure that MariaDB server enables geo-redundant backups"
+        id = "CKV_AZURE_129"
+        supported_resources = ['azurerm_mariadb_server']
+        categories = [CheckCategories.BACKUP_AND_RECOVERY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        #Whether or not public network access is allowed for this server. Defaults to true. Which is not optimal
+        if 'geo_redundant_backup_enabled' not in conf: 
+            return CheckResult.FAILED
+        else:
+            if  conf['geo_redundant_backup_enabled'][0]:
+                return CheckResult.PASSED
+            else:
+                return CheckResult.FAILED
+
+check = MariaDBGeoBackupEnabled()
