c41775447e4b17cc50cc39a602fbea4d2f86e34b
Nir Adler <nir@bridgecrew.io>
2021-03-17 17:16:22 +0200

    4x

---
+++ checkov/kubernetes/checks/KubeletReadOnlyPort.py
@@ -0,0 +1,49 @@
+
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.kubernetes.base_spec_check import BaseK8Check
+
+
+def commandsParser(conf):
+    keys=[]
+    values=[]
+    if "command" in conf:
+        for cmd in conf["command"]:
+            if "=" in cmd:
+                firstEqual = cmd.index("=")
+                [key, value] = [cmd[:firstEqual], cmd[firstEqual+1:]]
+                keys.append(key)
+                values.append(value)
+            else:
+                keys.append(cmd)
+                values.append(None)
+                            
+    
+    return keys,values
+                  
+
+
+class KubeletReadOnlyPort(BaseK8Check):
+    def __init__(self):
+        # CIS-1.6 4.2.4
+        id = "CKV_K8S_141"
+        name = "Ensure that the --read-only-port argument is set to 0"
+        categories = [CheckCategories.KUBERNETES]
+        supported_entities = ['containers']
+        super().__init__(name=name, id=id, categories=categories,
+                         supported_entities=supported_entities)
+
+    def get_resource_id(self, conf):
+        return f'{conf["parent"]} - {conf["name"]}'
+
+    def scan_spec_conf(self, conf):
+        [keys,values] = commandsParser(conf)
+
+        if "kubelet" in keys:
+            if '--read-only-port' in keys and values[keys.index('--read-only-port')] == "0":
+                return CheckResult.PASSED
+            return CheckResult.FAILED
+
+        return CheckResult.PASSED
+
+
+check = KubeletReadOnlyPort()
