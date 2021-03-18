726b2caec7a472fa53b1e431ac90d8f4d0a96273
milkanas <74859494+milkanas@users.noreply.github.com>
2021-03-18 14:35:34 +0200

    Update MySQLEncryptionEnaled.py

--- checkov/terraform/checks/resource/azure/MySQLEncryptionEnaled.py
+++ checkov/terraform/checks/resource/azure/MySQLEncryptionEnaled.py
@@ -13,11 +13,5 @@ class MySQLEncryptionEnaled(BaseResourceValueCheck):
     def get_inspected_key(self):
         return 'infrastructure_encryption_enabled'
 
-    def get_expected_value(self):
-        """
-        Returns the default expected value, governed by provider best practices
-        """
-        return True
-
 
 check = MySQLEncryptionEnaled()
