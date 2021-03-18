4f108b4ee80ddd293922734e28aeafeca434921b
milkanas <74859494+milkanas@users.noreply.github.com>
2021-03-18 14:36:55 +0200

    Update MySQLTreatDetectionEnabled.py

--- checkov/terraform/checks/resource/azure/MySQLTreatDetectionEnabled.py
+++ checkov/terraform/checks/resource/azure/MySQLTreatDetectionEnabled.py
@@ -14,7 +14,5 @@ class MySQLTreatDetectionEnabled(BaseResourceValueCheck):
     def get_inspected_key(self):
         return "threat_detection_policy/enabled"
 
-    def get_expected_value(self):
-        return True
 
 check = MySQLTreatDetectionEnabled()
