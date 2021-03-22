200c63a6b97617b2d082d426d7cac62a166d4a42
milkanas <74859494+milkanas@users.noreply.github.com>
2021-03-22 14:51:06 +0200

    Update test_MariaDBGeoBackupEnabled.py

--- tests/terraform/checks/resource/azure/test_MariaDBGeoBackupEnabled.py
+++ tests/terraform/checks/resource/azure/test_MariaDBGeoBackupEnabled.py
@@ -30,6 +30,28 @@ class MariaDBGeoBackupEnabled(unittest.TestCase):
         resource_conf = hcl_res['resource'][0]['azurerm_mariadb_server']['example']
         scan_result = check.scan_resource_conf(conf=resource_conf)
         self.assertEqual(CheckResult.FAILED, scan_result)
+        
+    def test_failure_empty(self):
+        hcl_res = hcl2.loads("""
+            resource "azurerm_mariadb_server" "example" {
+            name                = var.server_name
+            location            = var.resource_group.location
+            resource_group_name = var.resource_group.name
+            administrator_login          = var.admin_login
+            administrator_login_password = random_string.password.result
+            sku_name   = "B_Gen5_2"
+            storage_mb = 5120
+            version    = "10.2"
+            auto_grow_enabled             = true
+            backup_retention_days         = 7
+            public_network_access_enabled = true
+            #test this i guess
+            ssl_enforcement_enabled = false
+        }
+                """)
+        resource_conf = hcl_res['resource'][0]['azurerm_mariadb_server']['example']
+        scan_result = check.scan_resource_conf(conf=resource_conf)
+        self.assertEqual(CheckResult.FAILED, scan_result)
 
     def test_success(self):
         hcl_res = hcl2.loads("""
