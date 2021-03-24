28c3975115c8f4c542858341a8d3655f798a2021
tronxd <tron47@gmail.com>
2021-03-24 20:17:22 +0200

    exists + not exists connection operators + UT

--- tests/graph/terraform/checks_infra/connection_solvers/connection_exist_solver/test_solver.py
+++ tests/graph/terraform/checks_infra/connection_solvers/connection_exist_solver/test_solver.py
@@ -17,12 +17,3 @@ class ConnectionSolver(TestBaseSolver):
         expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
 
         self.run_test(root_folder=root_folder, expected_results=expected_results)
-
-    def test_connection_not_found(self):
-        root_folder = '../../../resources/ec2_instance_network_interfaces'
-        check_id = "NetworkInterfaceForInstance"
-        should_pass = []
-        should_fail = ['aws_vpc.other_vpc']
-        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
-
-        self.run_test(root_folder=root_folder, expected_results=expected_results)
--- tests/graph/terraform/checks_infra/connection_solvers/connection_not_exist_solver/test_solver.py
+++ tests/graph/terraform/checks_infra/connection_solvers/connection_not_exist_solver/test_solver.py
@@ -9,15 +9,6 @@ class ConnectionSolver(TestBaseSolver):
         self.checks_dir = TEST_DIRNAME
         super(ConnectionSolver, self).setUp()
 
-    def test_connection_found(self):
-        root_folder = '../../../resources/ec2_instance_network_interfaces'
-        check_id = "NetworkInterfaceForInstance"
-        should_pass = ['aws_instance.foo']
-        should_fail = []
-        expected_results = {check_id: {"should_pass": should_pass, "should_fail": should_fail}}
-
-        self.run_test(root_folder=root_folder, expected_results=expected_results)
-
     def test_connection_not_found(self):
         root_folder = '../../../resources/ec2_instance_network_interfaces'
         check_id = "NoNetworkInterfaceForInstance"
