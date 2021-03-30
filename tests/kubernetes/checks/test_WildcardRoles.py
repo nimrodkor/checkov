08a6b74d1c256eae6566252a2ff8a4bf36919878
nimrodkor <nimrodkor@gmail.com>
2021-03-30 11:29:08 +0300

    Make test explicit

--- tests/kubernetes/checks/test_WildcardRoles.py
+++ tests/kubernetes/checks/test_WildcardRoles.py
@@ -16,10 +16,25 @@ class TestWildcardRoles(unittest.TestCase):
         report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
         summary = report.get_summary()
 
+        passing_resources = {
+            'Role.test-should-pass-3.test'
+        }
+        failing_resources = {
+            'Role.test-should-fail-1.test',
+            'Role.test-should-fail-2.test',
+            'ClusterRole.test-should-fail-3.test'
+        }
+
         self.assertEqual(summary['passed'], 1)
         self.assertEqual(summary['failed'], 3)
         self.assertEqual(summary['skipped'], 0)
         self.assertEqual(summary['parsing_errors'], 0)
+
+        passed_check_resources = set([c.resource for c in report.passed_checks])
+        failed_check_resources = set([c.resource for c in report.failed_checks])
+
+        self.assertEqual(passing_resources, passed_check_resources)
+        self.assertEqual(failing_resources, failed_check_resources)
 
 
 if __name__ == '__main__':
