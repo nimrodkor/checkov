d328e1998c76f7147aac64d3c4c19364ad85a378
barak <schosterbarak@gmail.com>
2021-03-31 15:17:31 +0300

    fix tests

--- checkov/dockerfile/base_registry.py
+++ checkov/dockerfile/base_registry.py
@@ -6,14 +6,8 @@ class Registry(BaseCheckRegistry):
     def __init__(self):
         super().__init__()
 
-    def extract_entity_details(self, entity):
-        if isinstance(entity, list) and entity:
-            instruction = entity[0]['instruction']
-            return instruction, instruction, entity
 
     def scan(self, scanned_file, entity, skipped_checks, runner_filter):
-
-        # (entity_type, entity_name, entity_configuration) = self.extract_entity_details(entity)
 
         results = {}
         if not entity:
@@ -30,16 +24,8 @@ class Registry(BaseCheckRegistry):
                         entity_name = instruction
                         entity_type = instruction
                         entity_configuration = entity[instruction]
-                        result = self.run_check(check, entity_configuration, entity_name, entity_type, scanned_file,
-                                                skip_info)
-                        results[check] = {}
-                        if result['result'] == CheckResult.SKIPPED:
-                            results[check]['result'] = result['result']
-                            results[check]['suppress_comment'] = result['suppress_comment']
-                            results[check]['results_configuration'] = None
-                        else:
-                            results[check]['result'] = result['result'][0]
-                            results[check]['results_configuration'] = result['result'][1]
+                        self.update_result(check, entity_configuration, entity_name, entity_type, results, scanned_file,
+                                           skip_info)
 
         for check in self.wildcard_checks["*"]:
             if skipped_checks:
@@ -50,14 +36,18 @@ class Registry(BaseCheckRegistry):
                 entity_name = scanned_file
                 entity_type = "*"
                 entity_configuration = entity
-                result = self.run_check(check, entity_configuration, entity_name, entity_type, scanned_file,
-                                        skip_info)
-                results[check] = {}
-                if result['result'] == CheckResult.SKIPPED:
-                    results[check]['result'] = result['result']
-                    results[check]['suppress_comment'] = result['suppress_comment']
-                    results[check]['results_configuration'] = None
-                else:
-                    results[check]['result'] = result['result'][0]
-                    results[check]['results_configuration'] = result['result'][1]
+                self.update_result(check, entity_configuration, entity_name, entity_type, results, scanned_file,
+                                   skip_info)
         return results
+
+    def update_result(self, check, entity_configuration, entity_name, entity_type, results, scanned_file, skip_info):
+        result = self.run_check(check, entity_configuration, entity_name, entity_type, scanned_file,
+                                skip_info)
+        results[check] = {}
+        if result['result'] == CheckResult.SKIPPED:
+            results[check]['result'] = result['result']
+            results[check]['suppress_comment'] = result['suppress_comment']
+            results[check]['results_configuration'] = None
+        else:
+            results[check]['result'] = result['result'][0]
+            results[check]['results_configuration'] = result['result'][1]
